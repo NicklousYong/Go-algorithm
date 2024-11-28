909d750bda2cf040d6a0b45de4668862a79f2548
李翔 <7510909+li_xiang_Nicklous@user.noreply.gitee.com>
2024-11-28 19:30:07 +0800

    feat:找零钱问题，头皮发麻，要注意缩短零钱区间。

--- src/main/java/com/nicklous/algorithm/atraman/CoinChange_103.java
+++ src/main/java/com/nicklous/algorithm/atraman/CoinChange_103.java
@@ -14,10 +14,14 @@ public class CoinChange_103 {
         System.out.println("result is: "+result);
     }
 
+    public static int coinChange(int[] coins,int amount){
+        if(amount<0){
+            return -1;
+        }
+        return coinChange(coins,amount,new int[amount]);
+    }
 
-   static Map<Integer,Integer> minMap = new HashMap<Integer, Integer>();
-
-    public static int coinChange(int[]  coins,int amount){
+    public  static int coinChange(int[] coins, int amount,int[] count) {
         // 最优子结构 逆向思维，从答案反推解决问题
         if(amount<0){
             return -1;
@@ -25,25 +29,20 @@ public class CoinChange_103 {
         if(amount==0){
             return 0;
         }
+        if(count[amount-1] != 0){
+            return count[amount-1];
+        }
         //结果
         int min = Integer.MAX_VALUE;
         for (int coin : coins) {
-            if(minMap.containsKey(amount)){
-                return minMap.get(amount);
-            }else {
-                int result = coinChange(coins,amount-coin);
-                if(result>=0 && result<= min){
-                    min = result+1;
-                    minMap.put(amount,min==Integer.MAX_VALUE?-1:min);
-                }
-                return minMap.get(amount);
+            int result = coinChange(coins,amount-coin,count);
+            if(result>=0 && result< min){
+                min = result+1;
             }
         }
-
+        count[amount-1] = min==Integer.MAX_VALUE?-1:min;
+        return count[amount-1];
     }
-
-
-
 
 
 
